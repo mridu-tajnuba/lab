d1f5f72ebadf088b4ead0d31b2f3ca4a9c830d06
Tajnuba nahar mridu <60995072+mridu-tajnuba@users.noreply.github.com>
2020-06-11 11:47:47 +0600

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1; i>=1; i++) {
+
+    for(int j=1; j>=1; j++)
+
+        cout<<" MUHAMMAD TASNIM HOSSAIN MEHRAJ:"<<endl;
+    }
+    return 0;
+}
